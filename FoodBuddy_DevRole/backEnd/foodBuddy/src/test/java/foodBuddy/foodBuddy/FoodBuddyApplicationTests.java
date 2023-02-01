0170f9121deb3d2d8863dfeb55c43f2ec091a218
Kothwal shaik Sujahid Basha <kt515885@dal.ca>
2023-02-01 16:30:35 -0400

    springboot initialized

---
+++ FoodBuddy_DevRole/backEnd/foodBuddy/src/test/java/foodBuddy/foodBuddy/FoodBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package foodBuddy.foodBuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
