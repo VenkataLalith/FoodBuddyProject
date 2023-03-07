337b13656734e5d96d7207af635276c7e0a6fc65
Ashish Ojha <ashish.ojha@dal.ca>
2023-03-07 10:46:58 -0400

    Updated css for the application

--- FoodBuddy_DevRole/FrontEnd/src/Components/Layout.js
+++ FoodBuddy_DevRole/FrontEnd/src/Components/Layout.js
@@ -5,7 +5,7 @@ import './Layout.css';
 const Layout = ({ children }) => {
   return (
     <div>
-      <nav style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center', backgroundColor: 'rgb(162, 162, 244)', margin: 0, padding: '1rem' }}>
+      <nav style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center', backgroundColor: '#e6e6e6', margin: 0, padding: '1rem' }}>
         {/* Navigation bar content */}
         <ul style={{ listStyle: 'none', display: 'flex', gap: '1rem', margin: 0, padding: 0 }}>
           <li><a href="/">Home</a></li>
@@ -16,7 +16,7 @@ const Layout = ({ children }) => {
       <main style={{ height: '100%', marginBottom: '2rem', display: 'flex' }}>
         {children}
       </main>
-      <footer style={{ backgroundColor: 'rgb(162, 162, 244)', margin: 0, padding: '1rem' }}>
+      <footer style={{ backgroundColor: '#e6e6e6', margin: 0, padding: '1rem' }}>
         {/* Footer content */}
         <p>FoodBuddy App</p>
       </footer>
