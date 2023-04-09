39af6ba907301480e96a3423e77737529ae4b595
Kothwal shaik Sujahid Basha <kt515885@dal.ca>
2023-04-09 13:24:29 -0300

    added targed port for front end

--- FoodBuddy_DevRole/FrontEnd/src/App.js
+++ FoodBuddy_DevRole/FrontEnd/src/App.js
@@ -9,7 +9,7 @@ import { GroupManagement } from './Components/GroupManagement';
 
 function App() {
   axios.interceptors.request.use(config => {
-    config.headers['Origin'] = 'http://csci5308vm14.research.cs.dal.ca:8080';
+    config.headers['Origin'] = 'http://172.17.0.203';
     return config;
   });
 
